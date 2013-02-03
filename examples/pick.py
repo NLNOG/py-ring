bd7ea8c2bb4c7f9fb4405c358c4b6ac3f43d7bd6
Teun Vink <github@teun.tv>
2013-02-03 14:35:56 +0100

    more picking samples

--- examples/pick.py
+++ examples/pick.py
@@ -15,11 +15,13 @@ except ImportError:
 
 
 # let's pick nodes
-#ring.pick_nodes(5, include_hosts="bit01") 
-#ring.pick_nodes(5, include_networks="amazon") 
-#ring.pick_nodes(5, include_countries="fr") 
-#ring.pick_nodes(5, support_ipv6_only=True) 
-#ring.pick_nodes(5, support_ipv4=True)
-
-ring.pick_nodes(5, include_countries=['fr','be'], exclude_countries=['nl'], include_networks='claranet', include_hosts='bit01')
+print "5 nodes: %s " % ring.pick_nodes(5) 
+print "5 nodes, including bit01: %s" % ring.pick_nodes(5, inc_hosts="bit01") 
+print "5 nodes, including one from amazon: %s" % ring.pick_nodes(5, inc_networks="amazon") 
+print "5 nodes, including one in france: %s" % ring.pick_nodes(5, inc_countries="fr") 
+print "5 nodes, all ipv6 only: %s " % ring.pick_nodes(5, support_ipv6_only=True) 
+print "5 nodes, don't care about IPv6: %s " % ring.pick_nodes(5, support_ipv6_only=False) 
+print "5 nodes, all dual stack: %s " % ring.pick_nodes(5, support_ipv4=True)
+print "5 nodes, no IPv4 support: %s " % ring.pick_nodes(5, support_ipv4=False)
+print "5 nodes, ipv6 only + dual stack: %s " % ring.pick_nodes(5, support_ipv4=True, support_ipv6_only=True)
 
