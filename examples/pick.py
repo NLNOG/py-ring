958c37731ee408a44d4dca80a7a71ec56eae4829
Teun Vink <github@teun.tv>
2013-02-03 17:39:40 +0100

    minor changes

--- examples/pick.py
+++ examples/pick.py
@@ -25,6 +25,7 @@ print "max 5 nodes, all ipv6 only: %s " % ring.pick_nodes(5, support_ipv6_only=T
 print "max 5 nodes, don't care about IPv6: %s " % ring.pick_nodes(5, support_ipv6_only=False) 
 print "max 5 nodes, all dual stack: %s " % ring.pick_nodes(5, support_ipv4=True)
 print "max 5 nodes, no IPv4 support: %s " % ring.pick_nodes(5, support_ipv4=False)
+
 # this one should never give any results
 print "max 5 nodes, ipv6 only + dual stack: %s " % ring.pick_nodes(5, support_ipv4=True, support_ipv6_only=True) 
 
@@ -32,5 +33,3 @@ print "max 5 nodes, ipv6 only + dual stack: %s " % ring.pick_nodes(5, support_ip
 print "max 5 nodes, ipv6 only and not in .nl: %s" % ring.pick_nodes(5, support_ipv6_only=True, ex_countries='nl')
 print "max 5 nodes, at least one from claranet and one in belgium: %s" % ring.pick_nodes(5, inc_networks="claranet", inc_countries="be")
 print "max 5 nodes, only from japan and poland: %s" % ring.pick_nodes(5, only_countries=["jp","pl"])
-
-print ring.pick_nodes(5, inc_host="qq")
