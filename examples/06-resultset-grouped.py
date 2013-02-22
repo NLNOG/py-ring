bbc5cee0a2d849fb6409d2cc965921105e3e39fa
Teun Vink <github@teun.tv>
2013-02-22 22:13:34 +0100

    clearer output

--- examples/06-resultset-grouped.py
+++ examples/06-resultset-grouped.py
@@ -19,16 +19,19 @@ def callback(result):
         result.add_value('dig', result.get_stdout()[-1])
 
 
+# the command to be executed
+COMMAND = "dig +short -t a www.facebook.com"
+
 # pick random nodes
 nodes = ring.pick_nodes(50)
 
 # run the 'dig' command on the selected hosts with 'callback' as the analyse function
 # using www.facebook.com as a target since it has many A labels
-cmd_result = ring.run_command('dig -t a +short www.facebook.com', nodes, analyse=callback)
+cmd_result = ring.run_command(COMMAND, nodes, analyse=callback)
 
 # get the data for the 'dig' field grouped by value
 grouped = cmd_result.get_value_grouped('dig', True)
-print "hosts grouped by result:"
+print "results for '%s' grouped by return value:" % COMMAND
 print " #  result            hosts"
 for (value, hosts) in grouped:
     print "%2d: %-16s %s" % (len(hosts), value, ", ".join(hosts))
